eb7c25c81654684285d623138fcd6e97d466e96f
Alyssa DeMetro <leanndemetro@gmail.com>
2021-07-01 10:07:50 -0400

    mock data added to local storage values within init function for development version only

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ init();
 function init() {
   // create a method to parse out the local storage item called totalRaised
   var displayedRaised = localStorage.getItem("totalRaised");
+ // create a local storage item that will store the client's name
+ localStorage.setItem("clientName", "the-water-project");
+ //log this data to the console
+ console.log("clientName: " + localStorage.getItem("clientName"));
+ // create a local storage item that will store the client's donation goal
+ localStorage.setItem("donationGoal", "2000");
+
+  var displayedRaised = localStorage.getItem("totalRaised");
   //append it to the widget-total-funds element (with a dollar sign)
   if (displayedRaised !== null) {
     $("#widget-total-funds").text("$" + displayedRaised + ".00");
